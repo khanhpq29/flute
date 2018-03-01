ec06380ef4c0692a236cf4129483d9bb0abe5170
Phan Khanh <khanhpq@Phongs-Mac-mini.local>
2018-03-01 10:01:55 +0700

    add bottom sheet

--- lib/layoutfile.dart
+++ lib/layoutfile.dart
@@ -1,39 +1,137 @@
-//body: new Container( //begin container
-//padding: const EdgeInsets.all(16.0),
-//child: new Row(
-//children: [
-//new Expanded(
-//child: new Column(
-//crossAxisAlignment: CrossAxisAlignment.start,
-//children: [
-//new Container(
-//padding: const EdgeInsets.only(bottom: 8.0),
-//child: new Text(
-//'Oeschinen Lake Campground',
-//style: new TextStyle(
-//fontWeight: FontWeight.bold,
-//),
-//),
-//),
-//new Text(
-//'Kandersteg, Switzerland',
-//style: new TextStyle(
-//color: Colors.grey[500],
-//),
-//),
-//],
-//),
-//),
-//new Column(
-//children: <Widget>[
-//new Icon(
-//Icons.star,
-//color: Colors.red[500],
-//),
-//new Text('41'),
-//],
-//)
-//
-//],
-//),
-//),//end container
+import 'package:flutter/material.dart';
+
+class ScreenChatState extends State {
+  double billAmount = 0.0;
+  double tipPercentage = 0.0;
+  var total = 0.0;
+  AlertDialog alertDialog;
+  Text tvSum;
+  TextEditingController controller = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Simple calculator"),
+      ),
+      body: new Container(
+        child: new Column(
+          children: <Widget>[
+            createBuildAmount(),
+            createTipPercentage(),
+            createRaisedButton(),
+            createTvSum(),
+            createChangeButton()
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget createBuildAmount() {
+    return new TextField(
+      keyboardType: TextInputType.number,
+      decoration: new InputDecoration(
+          hintText: "Bill amount"
+      ),
+      controller: controller,
+      onChanged: (String value) {
+        try {
+          billAmount = double.parse(value);
+        } catch (exception) {
+          billAmount = 0.0;
+        }
+      },
+    );
+  }
+
+  Widget createTipPercentage() {
+    return new TextField(
+      keyboardType: TextInputType.number,
+      decoration: new InputDecoration(
+          hintText: "Percentage"
+      ),
+      onChanged: (String value) {
+        try {
+          tipPercentage = double.parse(value);
+        } catch (exception) {
+          tipPercentage = 0.0;
+        }
+      },
+    );
+  }
+
+  Widget createRaisedButton() {
+    return new Container(
+        padding: const EdgeInsets.only(top: 16.0),
+        child: new RaisedButton(
+            child: new Text("Add", overflow: TextOverflow.ellipsis,),
+            onPressed: () {
+              handlePressed();
+            }
+        )
+    );
+  }
+
+  Widget createChangeButton() {
+    return new Builder(
+      builder: (BuildContext context) {
+        return new Center(
+          child: new RaisedButton(
+            child: new Text('SHOW A SNACKBAR'),
+            onPressed: () {
+              Scaffold.of(context).showSnackBar(new SnackBar(
+                content: new Text('Hello!'),
+                action: new SnackBarAction(label: "OK", onPressed: handlePressSnackBar),
+              ));
+            },
+          ),
+        );
+      },
+    );
+  }
+
+  void handlePressed() {
+    var caltip = billAmount * tipPercentage / 100;
+    total = caltip + billAmount;
+    alertDialog = new AlertDialog(
+      title: new Text("demo alert"),
+      content: new Text("Bill amount: \$$billAmount \n"
+          "Total: \$$total"),
+      actions: <Widget>[
+        new FlatButton(onPressed: pressOk, child: new Text("Ok")),
+        new FlatButton(onPressed: pressCancel, child: new Text("Cancel"))
+      ],
+    );
+    showDialog(context: context, child: alertDialog);
+  }
+
+  Widget createTvSum() {
+    return new Container(
+      padding: const EdgeInsets.only(top: 16.0, bottom: 16.0),
+      child: tvSum = new Text("iefjieaj"),
+    );
+  }
+
+  void pressOk() {
+    Navigator.pop(context);
+  }
+
+  void pressCancel() {
+    Navigator.pop(context);
+  }
+
+  void changeText() {
+    SnackBar snackBar = new SnackBar(content: new Text("snack bar demo"),
+      action: new SnackBarAction(label: "OK", onPressed: () {
+        Scaffold.of(context).showSnackBar(
+            new SnackBar(content: new Text('new As action'),));
+      }),
+    );
+    Scaffold.of(context).showSnackBar(snackBar);
+  }
+
+  void handlePressSnackBar() {
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -16,142 +16,66 @@ class DemoLab extends StatelessWidget {
 class ScreenChat extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => new ScreenChatState();
-
 }
 
 class ScreenChatState extends State {
-  double billAmount = 0.0;
-  double tipPercentage = 0.0;
-  var total = 0.0;
-  AlertDialog alertDialog;
-  Text tvSum;
-  TextEditingController controller = new TextEditingController();
-
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text("Simple calculator"),
+        title: new Text("chips_example"),
       ),
       body: new Container(
         child: new Column(
           children: <Widget>[
-            createBuildAmount(),
-            createTipPercentage(),
-            createRaisedButton(),
-            createTvSum(),
-            createChangeButton()
+            new Chip(
+              avatar: new CircleAvatar(
+                backgroundColor: Colors.blue,
+                child: new Text('AB'),
+              ),
+              label: new Text('Aaron Burr'),
+            ),
+            new Divider(
+              height: 12.0,
+              color: Colors.redAccent,
+            ),
+            new RaisedButton(
+              onPressed: handlePress,
+              child: new Text("show"),
+            ),
           ],
         ),
       ),
     );
   }
 
-  Widget createBuildAmount() {
-    return new TextField(
-      keyboardType: TextInputType.number,
-      decoration: new InputDecoration(
-          hintText: "Bill amount"
-      ),
-      controller: controller,
-      onChanged: (String value) {
-        try {
-          billAmount = double.parse(value);
-        } catch (exception) {
-          billAmount = 0.0;
-        }
-      },
-    );
+  void handlePress() {
+    showModalBottomSheet<Null>(
+        context: context, builder: (BuildContext context) {
+      return new Container(
+        margin: const EdgeInsets.symmetric(horizontal: 9.0),
+        padding: const EdgeInsets.only(top: 16.0, bottom: 16.0),
+        child: new Row(
+          children: <Widget>[
+            new Flexible(
+                child: new TextField(
+                  decoration: new InputDecoration.collapsed(hintText: "Tap to send a message"),
+                )
+            ),
+            new Container(
+              margin: const EdgeInsets.symmetric(horizontal: 5.0),
+              child: new FlatButton(
+                  onPressed: handleButton,
+                  child: new Text("Ok")
+              )
+              ,)
+          ],
+        ),
+      );
+    });
   }
 
-  Widget createTipPercentage() {
-    return new TextField(
-      keyboardType: TextInputType.number,
-      decoration: new InputDecoration(
-          hintText: "Percentage"
-      ),
-      onChanged: (String value) {
-        try {
-          tipPercentage = double.parse(value);
-        } catch (exception) {
-          tipPercentage = 0.0;
-        }
-      },
-    );
-  }
-
-  Widget createRaisedButton() {
-    return new Container(
-        padding: const EdgeInsets.only(top: 16.0),
-        child: new RaisedButton(
-            child: new Text("Add", overflow: TextOverflow.ellipsis,),
-            onPressed: () {
-              handlePressed();
-            }
-        )
-    );
-  }
-
-  Widget createChangeButton() {
-    return new Builder(
-      builder: (BuildContext context) {
-        return new Center(
-          child: new RaisedButton(
-            child: new Text('SHOW A SNACKBAR'),
-            onPressed: () {
-              Scaffold.of(context).showSnackBar(new SnackBar(
-                content: new Text('Hello!'),
-                action: new SnackBarAction(label: "OK", onPressed: handlePressSnackBar),
-              ));
-            },
-          ),
-        );
-      },
-    );
-  }
-
-  void handlePressed() {
-    var caltip = billAmount * tipPercentage / 100;
-    total = caltip + billAmount;
-    alertDialog = new AlertDialog(
-      title: new Text("demo alert"),
-      content: new Text(controller.text),
-//      content: new Text("Bill amount: \$$billAmount \n"
-//          "Total: \$$total"),
-      actions: <Widget>[
-        new FlatButton(onPressed: pressOk, child: new Text("Ok")),
-        new FlatButton(onPressed: pressCancel, child: new Text("Cancel"))
-      ],
-    );
-    showDialog(context: context, child: alertDialog);
-  }
-
-  Widget createTvSum() {
-    return new Container(
-      padding: const EdgeInsets.only(top: 16.0, bottom: 16.0),
-      child: tvSum = new Text("iefjieaj"),
-    );
-  }
-
-  void pressOk() {
-    Navigator.pop(context);
-  }
-
-  void pressCancel() {
-    Navigator.pop(context);
-  }
-
-  void changeText() {
-    SnackBar snackBar = new SnackBar(content: new Text("snack bar demo"),
-      action: new SnackBarAction(label: "OK", onPressed: () {
-        Scaffold.of(context).showSnackBar(
-            new SnackBar(content: new Text('new As action'),));
-      }),
-    );
-    Scaffold.of(context).showSnackBar(snackBar);
-  }
-
-  void handlePressSnackBar() {
+  void handleButton() {
   }
 }
